0747b54221e74ce85bfc7ce63a83e3864ba200b6
yuukiokuda <136579520+yuukiokuda@users.noreply.github.com>
2023-07-07 00:04:33 +0900

    soldoutの表示と条件分岐

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,12 @@
 class OrdersController < ApplicationController
+  before_action :select_item, only: [:index, :create]
+
   def index
     @order_address = OrderAddress.new
     # @orders = Order.all
-    @item = Item.find(params[:item_id])
   end
 
   def create
-    # binding.pry
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       @order_address.save
@@ -22,4 +22,7 @@ class OrdersController < ApplicationController
     params.require(:order_address).permit(:postal_code, :prefecture, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
+  def select_item
+    @item = Item.find(params[:item_id])
+  end
 end
