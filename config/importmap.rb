c70004c5d27c09acdaca13b61cbd805f1f4f70b3
yuukiokuda <136579520+yuukiokuda@users.noreply.github.com>
2023-07-12 15:19:17 +0900

    メンター修正補足

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,6 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "application", preload: true
-pin "price", to: "price.js"
+pin "application", preload: true   #メンター
+pin "price", to: "price.js"        #メンター
 pin "card", to: "card.js"
